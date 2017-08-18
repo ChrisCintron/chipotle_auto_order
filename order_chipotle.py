17759dbda39a0d28195608e50a399fdba7e91e73
Chris Cintron <chriscintron502@gmail.com>
2017-08-18 16:12:18 -0500

    Code runs through
    Completes order. Need to unhashtag the last driver submit if you want
    the code to order.

--- order_chipotle.py
+++ order_chipotle.py
@@ -27,32 +27,44 @@ driver = webdriver.Chrome()
 driver.get(url)
 
 
-
-driver.find_element_by_id('headerGreeting').click()
-driver.find_element_by_id('email').send_keys(info['Chris']['email'])
-driver.find_element_by_id('password-input').send_keys(info['Chris']['password'])
-driver.find_element_by_id('sign-in').click()
-time.sleep(1)
-driver.find_element_by_name("BURRITO BOWL").click()
-time.sleep(1)
+def login():
+    driver.find_element_by_id('headerGreeting').click()
+    driver.find_element_by_id('email').send_keys(info['Chris']['email'])
+    driver.find_element_by_id('password-input').send_keys(info['Chris']['password'])
+    driver.find_element_by_id('sign-in').click()
+    time.sleep(1)
+    driver.find_element_by_name("BURRITO BOWL").click()
+    time.sleep(1)
 
 def pick_menu(menu, specific_order):
     for ingredients in specific_order:
         time.sleep(1)
         driver.find_element_by_id(menu[ingredients]).click()
 
-
 def checkout():
+    #Add to bags continue
     driver.find_element_by_xpath('//*[@id="elevator"]/div[3]/cmg-arrow-button').click()
     time.sleep(2)
     driver.find_element_by_xpath('//*[@id="bag-container"]/div[5]/div[2]/button[2]').click()
     time.sleep(1)
+    #Pickup Restaurant
     driver.find_element_by_class_name('btn-checkout-continue').click()
     time.sleep(1)
-    #pickup_time = driver.find_element_by_name('select name').text
-    #print(pickup_time)
+    #Pickup Time
+    pickup_time = driver.find_element_by_name('select name').text[0:4]
+    driver.find_element_by_class_name('btn-checkout-continue').click()
+    time.sleep(1)
+    #Payment
+    driver.find_element_by_class_name('btn-checkout-continue').click()
+    time.sleep(1)
+    #Submit button
+    #This line is hashed for testing
+    #driver.find_element_by_id('submit-order').click()
+    print('Pickup-Time: ' + pickup_time)
 
 
 
-pick_menu(menu, info['Chris']['order'])
-checkout()
+if __name__ == '__main__':
+    login()
+    pick_menu(menu, info['Chris']['order'])
+    checkout()
